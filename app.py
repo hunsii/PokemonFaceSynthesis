886e336dc660bab84d11e7d46afc908e28831163
Jin Sihun <94668480+hunsii@users.noreply.github.com>
2023-06-14 15:21:53 +0900

    Final

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 import torch
 
+import timm
 
 """style mix"""
 import cv2
@@ -18,6 +19,42 @@ from mediapipe.framework.formats import landmark_pb2
 # import numpy as np
 import argparse
 import gradio as gr
+import glob
+
+from torchvision import transforms
+import ast
+
+def get_pokemon_point_list():
+    l = glob.glob("points/*.txt")
+    data_dict = {}
+    data = ""
+    for path in l:
+        with open(path, "r") as f:
+            data+= f.read().strip()
+    datas = data.split("-"*19)
+    for data_dump in datas:
+        # print(data_dump)
+        # print('-'*80)
+        lines = data_dump.strip().splitlines()
+        if len(lines) == 0:
+            continue
+        basename = lines[0].split("\\")[-1]
+        # print(f"basename: {basename}")
+
+        points = []
+        # (x, y) 형태의 숫자 데이터를 (x, y) 튜플로 파싱하여 리스트에 저장합니다.
+        for idx in [1, 4, 6, 5, 2, 3]:
+            line = lines[idx]
+            line = line.replace("(", "").replace(")", "")
+            points.append([int(v) for v in line.split(", ")])
+        
+        # 파싱된 데이터를 NumPy 배열로 변환합니다.
+        numpy_array = np.array(points)
+        # print(numpy_array)
+
+        # data_dict = {}
+        data_dict[basename.split('_')[0]] = numpy_array
+    return data_dict
 
 def get_facearea(image):
     # Predefined indices for specific landmarks of interest
@@ -130,21 +167,27 @@ def get_center_of_mask(mask):
     center_x = np.mean(white_pixels[1]).astype(np.int16)  # Average of y-coordinates
     return center_x, center_y
 
-def get_homography_pokemon(image, pokemon, pts1):
-    pts2 = np.array([
-        [[198,  83]], 
-        [[294, 162]],
-        [[144, 140]],
-        [[170, 206]],
-        [[288, 110]], 
-        [[225, 220]],
-    ])
-
+def get_homography_pokemon(image, pokemon, pts1, pts2):
     H, _ = cv2.findHomography(pts2, pts1, cv2.RANSAC) # pts1과 pts2의 행렬 주의 (N,1,2)
     result3 = cv2.warpPerspective(pokemon, H, (image.shape[1], image.shape[0]))
     return result3
 
 def main(opt):
+    model = timm.create_model('efficientnet_b0', pretrained=True)
+    model.load_state_dict(torch.load("saved_model_b0.pth", map_location=opt.device))
+    model.eval()
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    # 파일에서 문자열 읽기
+    with open("pokemon_mapping.txt", "r") as file:
+        data = file.readlines()[0].split('=',maxsplit=2)[-1]#.split('\n')[0]
+    # 문자열을 dict 형태로 변환
+    pokemon_mapping_dict = ast.literal_eval(data)
+
     model2 = torch.hub.load(
         "AK391/animegan2-pytorch:main",
         "generator",
@@ -158,35 +201,48 @@ def main(opt):
         size=512, device=opt.device, side_by_side=False
     )
 
+    pokemon_point_list = get_pokemon_point_list()
+
     def greet(image):
-        # Find the most similar pokemon.
-        pokemon_path = "images/Mew/Mew_000.png" # path_list[rnd_idx]
-        pokemon = cv2.imread(pokemon_path)
+        cv2.imwrite("temp.png", image)
+        # Convert from openCV2 to PIL for AnimeGAN2
+        image_pil = Image.fromarray(image)
+        image_transformed = transform(image_pil).unsqueeze(0)
+
+        with torch.no_grad():
+            outputs = model(image_transformed)
+            
+        # 가장 높은 값을 가진 클래스 선택
+        _, predicted = torch.max(outputs, 1)
+
+        pokemon_name = pokemon_mapping_dict[str(predicted.detach().numpy()[0])]
+
+        pokemon = cv2.imread(f"images/{pokemon_name}/{pokemon_name}_000.png")
         pokemon = cv2.resize(pokemon, (512, 512))
-        pokemon_name = "뮤츠"
+
         result_sentence = f"당신과 가장 닮은 포켓몬은 {pokemon_name}입니다!"
 
-        # Make mask image from user image.
+        # Make mask image and landmarks from user image.
         mask, pts1 = get_facearea(image=image)
-
-        # Convert from RGB to BGR (opencv use rgb channel).
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Get center point of mask image.
         center_of_mask = get_center_of_mask(mask)
 
+        # Get pokemon landmarks
+        pts2 = pokemon_point_list[pokemon_name]
+
         # Apply homography from two point sets
-        result = get_homography_pokemon(image=image, pokemon=pokemon, pts1=pts1)
+        result = get_homography_pokemon(image=image, pokemon=pokemon, pts1=pts1, pts2=pts2)
 
         # Blend user image and pokemon image.
         # For more quility, i will use saemlessclone.
         mixed = cv2.seamlessClone(result, image, mask, center_of_mask, cv2.NORMAL_CLONE)
         
         # Convert from BGR to RGB
-        color_coverted = cv2.cvtColor(mixed, cv2.COLOR_BGR2RGB)
+        # color_coverted = cv2.cvtColor(mixed, cv2.COLOR_BGR2RGB)
 
         # Convert from openCV2 to PIL for AnimeGAN2
-        result=Image.fromarray(color_coverted)
+        result = Image.fromarray(mixed)
         result = face2paint(model2, result)
 
         # Convert from openCV2 to PIL for AnimeGAN2
@@ -208,7 +264,7 @@ def main(opt):
         title="딥러닝 프로그래밍 기말 프로젝트",
         description="당신과 가장 닮은 포켓몬을 찾아보세요!",
         article = "<p style='text-align: center'><a href='https://github.com/hunsii/PokemonFaceSynthesis' target='_blank'>Github Repo by hunsii</a></p> <center></center></p>",
-        # examples=[['input.png', 'True'], ['input.png', 'True']],
+        examples=[['26000.png']],
         # live=True,
     )
     demo.launch()
